d71608b07e2046a07b805a20ae5e164a73235722
ashwinimane97 <mane.ashwinik@gmail.com>
2025-03-07 13:43:07 +0530

    Revert "Feedback"

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
-require('dotenv').config();
 const express = require('express');
 const app = express();
-const taskRoutes = require('./routes/tasksRoutes')
-
-const port = process.env.PORT;
+const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/tasks', taskRoutes);
 
 app.listen(port, (err) => {
     if (err) {
